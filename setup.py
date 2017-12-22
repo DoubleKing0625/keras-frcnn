fe1f7b946cb0227845b6ba0f97fc40b09a6c7ae5
DoubleKing <kevin.pqx@outlook.com>
2017-12-22 09:31:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='keras_frcnn',
     description='faster_rcnn implementation in Keras',
-    url='https://github.com/DoubleKing0625/keras_frcnn',
+    url='https://github.com/DoubleKing0625/keras-frcnn',
     license='GPLv3',
     packages=find_packages()
 )
